88258be663c882aacf47ba67fd2b761673066327
sumori <hoangthiphuonguyen1602@gmail.com>
2022-03-27 23:52:55 +0700

    demogithub

---
+++ C# PRACTICE/interface.cs
@@ -0,0 +1,31 @@
+// using System;
+// namespace _NET
+// {
+//     interface IVehicle
+//     {
+//         void Drive_Type();
+//         bool Fuel_Type(int fuel);
+//     }
+//     class Car : IVehicle
+//     {
+//         private int _fuel;
+//         public Car(int fuel)
+//         {
+//             _fuel = fuel;
+//         }
+//         public void Drive_Type()
+//         {
+//             if (_fuel >0)
+//             Console.WriteLine("The Car is driving");
+//             else
+//                 Console.WriteLine("The Car is out of gasoline");
+//         }
+//         public bool Fuel_Type(int fuel)
+//         {
+//             _fuel = fuel;
+//             return true;
+//         }
+//     }
+
+// }
+
